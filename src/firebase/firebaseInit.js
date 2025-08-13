e248635c39b10483021c153c2e4aedd295187091
Fabrício Ricard <fabricioricard23@gmail.com>
2025-08-13 01:25:50 -0300

    Delete src directory

--- src/firebase/firebaseInit.js
+++
@@ -1,8 +0,0 @@
-import { auth, firestore } from './firebaseConfig';
-
-export const initializeFirebase = () => {
-  // Additional initialization logic if needed
-  firestore.settings({ ignoreUndefinedProperties: true });
-};
-
-export { auth, firestore };
